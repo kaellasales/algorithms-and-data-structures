5c343090ae9137581f26d65b34e57067c48f76b6
Kaella Sales <kaellasales09@gmail.com>
2026-08-17 14:58:34 -0300

    refactor(linked-list): use shared Node in Add Two Numbers

--- src/linked_list/leetcode/LeetCode002AddTwoNumbers.java
+++ src/linked_list/leetcode/LeetCode002AddTwoNumbers.java
@@ -1,45 +1,41 @@
 package linked_list.leetcode;
-class ListNode {
-    int val;
-    ListNode next;
-}
+
+import linked_list.Node;
 
 public class LeetCode002AddTwoNumbers {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
-        ListNode dummy = new ListNode();
-        ListNode current = dummy; // current recebe uma CÓPIA do ENDEREÇO
+    public Node addTwoNumbers(Node l1, Node l2) {
+
+        Node dummy = new Node(0);
+        Node current = dummy;
 
         int carry = 0;
 
         while (l1 != null || l2 != null) {
 
-            int x = (l1 != null) ? l1.val : 0;
-            int y = (l2 != null) ? l2.val : 0;
+            int x = (l1 != null) ? l1.value : 0;
+            int y = (l2 != null) ? l2.value : 0;
 
-            int soma = x + y + carry;
+            int sum = x + y + carry;
 
-            carry = soma / 10;
+            carry = sum / 10;
 
-            ListNode newNode = new ListNode();
-            newNode.val = soma % 10;
+            Node newNode = new Node(sum % 10);
 
-            current.next = newNode; // altera o objeto no endereço → dummy vê também
-
-            current = current.next; // current aponta pra outro endereço → dummy NÃO muda
+            current.next = newNode;
+            current = current.next;
 
             if (l1 != null) {
-                l1 = l1.next; // avança na lista linkada 1
+                l1 = l1.next;
             }
 
             if (l2 != null) {
-                l2 = l2.next; // avança na lista linkada 1
+                l2 = l2.next;
             }
         }
 
         if (carry > 0) {
-            current.next = new ListNode();
-            current.next.val = carry;
+            current.next = new Node(carry);
         }
 
         return dummy.next;
